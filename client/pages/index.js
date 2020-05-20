f5b5e05fa1dcfa9a4b543cacfc91cba6a5986ec4
Mikhail <michaildrazdovich@gmail.com>
2020-05-21 00:49:59 +0300

    End client side

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,30 +1,16 @@
-import React from 'react';
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>
+  return currentUser
+    ? <h1>You are signed in</h1>
+    : <h1>You are not signed in</h1>
 };
 
-LandingPage.getInitialProps = async () => {
-  console.log('I was executed');
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: {
-          Host: 'ticketing.dev'
-        }
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-
-  return {};
+  return data;
 };
 
 export default LandingPage;
