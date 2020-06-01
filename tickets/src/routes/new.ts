64a86c4808a5cd9f1333af4e47c2861793c541ea
Mikhail <michaildrazdovich@gmail.com>
2020-06-02 00:08:47 +0300

    Add orders

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -21,7 +21,6 @@ router.post('/api/tickets', requireAuth, [
             userId: req.currentUser!.id,
         });
         await ticket.save();
-
         await new TicketCreatedPublisher(natsWrapper.client).publish({
             id: ticket.id,
             title: ticket.title,
