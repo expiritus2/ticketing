1ccaedaa7851e1a047e4533ea57d6d51d77896b6
Mikhail <michaildrazdovich@gmail.com>
2020-06-07 00:04:46 +0300

    Publish orders

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+import { OrderStatus } from "@mddreamproject/common";
+import { natsWrapper } from "../../nats-wrapper";
 
 it('returns an error if the ticket does not exist', async () => {
     const ticketId = mongoose.Types.ObjectId();
@@ -12,10 +16,53 @@ it('returns an error if the ticket does not exist', async () => {
         .expect(404);
 });
 
-it('returns an error fi the ticket is already reserved', async () => {
+it('returns an error if the ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20,
+    });
+    await ticket.save();
+    const order = Order.build({
+        ticket,
+        userId: 'skdfjlsdf',
+        status: OrderStatus.Created,
+        expiresAt: new Date(),
+    });
+    await order.save();
 
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id })
+        .expect(400);
 });
 
 it('reserves a ticket', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
 
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id })
+        .expect(201);
+});
+
+it('emits an order created event', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
