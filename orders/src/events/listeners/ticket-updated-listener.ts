849135936811aa3f998bede17065889654b25f87
Mikhail <michaildrazdovich@gmail.com>
2020-06-08 23:39:46 +0300

    Orders tests

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -1,17 +1,14 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, Subjects, TicketUpdatedEvent } from "@mddreamproject/common";
 import { queueGroupName } from "./queue-group-name";
-import { Ticket } from "../../models/ticket";
+import { Ticket} from "../../models/ticket";
 
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
     subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
     queueGroupName = queueGroupName;
 
     async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-        const ticket = await Ticket.findOne({
-            _id: data.id,
-            version: data.version - 1,
-        });
+        const ticket = await Ticket.findByEvent(data);
 
         if (!ticket) {
             throw new Error('Ticket not found');
---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,12 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from "@mddreamproject/common";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName = queueGroupName;
+
+    onMessage(data: OrderCreatedEvent["data"], msg: Message): void {
+
+    }
+}
